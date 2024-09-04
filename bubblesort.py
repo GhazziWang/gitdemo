e8d7141141cc6033649958a0177b062c26935a83
Ghazzi_Wang <ghazziwang2000@outlook.com>
2024-09-04 16:21:23 +0200

    222

--- bubblesort.py
+++ bubblesort.py
@@ -4,6 +4,7 @@ def bubblesort(arr):
 
     :arr: unsorted list
     :return: sorted list
+    :22222
     """
     n = len(arr)
 
@@ -11,10 +12,10 @@ def bubblesort(arr):
     for i in range(n - 1):
 
         # Inner loop
+        # 222
         for j in range(n - 1 - i):
 
             # Swap elements
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-
 return arr
